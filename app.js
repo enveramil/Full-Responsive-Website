db2727b7c917b034e93c74268b1b5f6bf3732571
Enver Amil <enveramil@hotmail.com>
2022-02-20 17:52:58 +0300

    editing home content and resize

--- app.js
+++ app.js
@@ -11,4 +11,112 @@ $("#headermenuacmabutonalanı").on("click", function(){
 });
 
 
+/* Sayfanın genişliği alınacak */
+var siteWidth = $(window).outerWidth();
+if(! $("#headerMessageField").length){
+    if(siteWidth >= 1200){
+        var ofsetValue  = 108;
+
+    }
+    else if((siteWidth >= 992) && (siteWidth <= 1199)){
+        var ofsetValue  = 98;
+
+    }
+    else if((siteWidth >= 768) && (siteWidth <= 991)){
+        var ofsetValue  = 88;
+
+    }
+    else if((siteWidth >= 576) && (siteWidth <= 767)){
+        var ofsetValue  = 78;
+
+    }
+    else if((siteWidth <= 574)){
+        var ofsetValue  = 78;
+
+    }       
+
+}
+
+else{
+    if(siteWidth >= 1200){
+        var ofsetValue  = 148;
+
+    }
+    else if((siteWidth >= 992) && (siteWidth <= 1199)){
+        var ofsetValue  = 138;   
+
+    }
+    else if((siteWidth >= 768) && (siteWidth <= 991)){
+        var ofsetValue  = 118;
+
+    }
+    else if((siteWidth >= 576) && (siteWidth <= 767)){
+        var ofsetValue  = 108;
+
+    }
+    else if((siteWidth <= 574)){
+        var ofsetValue  = 103;
+
+    }       
+
+}
+
+jQuery("main").css("top",ofsetValue);
+jQuery("footer").css("top",ofsetValue);
+
+$(window).resize(function(){
+    var siteWidth = $(window).outerWidth();
+if(! $("#headerMessageField").length){
+    if(siteWidth >= 1200){
+        var ofsetValue  = 108;
+
+    }
+    else if((siteWidth >= 992) && (siteWidth <= 1199)){
+        var ofsetValue  = 98;
+
+    }
+    else if((siteWidth >= 768) && (siteWidth <= 991)){
+        var ofsetValue  = 88;
+
+    }
+    else if((siteWidth >= 576) && (siteWidth <= 767)){
+        var ofsetValue  = 78;
+
+    }
+    else if((siteWidth <= 574)){
+        var ofsetValue  = 78;
+
+    }       
+
+}
+
+else{
+    if(siteWidth >= 1200){
+        var ofsetValue  = 148;
+
+    }
+    else if((siteWidth >= 992) && (siteWidth <= 1199)){
+        var ofsetValue  = 138;   
+
+    }
+    else if((siteWidth >= 768) && (siteWidth <= 991)){
+        var ofsetValue  = 118;
+
+    }
+    else if((siteWidth >= 576) && (siteWidth <= 767)){
+        var ofsetValue  = 108;
+
+    }
+    else if((siteWidth <= 574)){
+        var ofsetValue  = 103;
+
+    }       
+
+}
+
+jQuery("main").css("top",ofsetValue);
+jQuery("footer").css("top",ofsetValue);
+
+});
+
 });
